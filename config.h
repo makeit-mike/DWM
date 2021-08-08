eb050970c25a9603eb8a2fd91a460dfe1111f049
makeit-mike <mike.isaac.jones@gmail.com>
2021-08-07 21:47:01 -0500

    adding in arch icon, adding in screenshot tool

--- config.h
+++ config.h
@@ -1,25 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 10;        /* border pixel of windows */
+static const unsigned int borderpx  = 20;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "terminus:size=30","fontawesome:size=30" };
+static const char *fonts[]          = { "terminus:size=20","fontawesome:size=30" };
 static const char dmenufont[]       = "terminus:size=20";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#222222";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#404040";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "" };
+static const char *tags[] = { "", "", "", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -41,7 +41,7 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "ﱖ",      tile },    /* first entry is default */
 	{ "",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      monocle },
 };
 
 /* key definitions */
@@ -65,6 +65,7 @@ static const char *chromecmd[] = { "google-chrome-beta", NULL };
 static const char *vscodecmd[] = { "code", NULL }; 
 // CLI Programs
 static const char *rangercmd[] = { "alacritty", "-e", "/usr/bin/ranger",NULL };
+static const char *screenshotcmd[] = { "alacritty", "-e", "/usr/bin/deepin-screenshot",NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -73,6 +74,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_v,      spawn,	   {.v = vscodecmd } },
 	{ MODKEY,			XK_w,      spawn,	   {.v = chromecmd } },
 	{ MODKEY,			XK_e,      spawn,	   {.v = rangercmd } },
+	{ MODKEY,			XK_s,      spawn,	   {.v = screenshotcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
